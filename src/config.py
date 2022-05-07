6dfaf14b72523b95d0cac906af64c8cebd2d5375
truong.hoang <truong.hoang@onemount.com>
2022-05-08 02:52:04 +0700

    create config

---
+++ src/config.py
@@ -0,0 +1,16 @@
+class Setting:
+    INTERVAL_DELAY = 0
+    MAX_RETRY = 10
+    BASE_URL = "https://www.imdb.com/"
+    USER_AGENT = 'Thunder Client (https://www.thunderclient.com)'
+    RAW_CACHE = './cache/raw_movie_data.json'
+    WRITERS_CACHE = './cache/writers.csv'
+    DIRECTORS_CACHE = './cache/directors.csv'
+    ACTORS_CACHE = './cache/actors.csv'
+    ACTORS_MOVIE_CACHE = './cache/actor_movie.csv'
+    DIRECTORS_MOVIE_CACHE = './cache/director_movie.csv'
+    WRITERS_MOVIE_CACHE = './cache/writers_movie.csv'
+    GENRES_MOVIE_CACHE = './cache/genres_movie.csv'
+    COUNTRY_MOVIE_CACHE = './cache/country_movie.csv'
+    LANGUAGE_MOVIE_CACHE = './cache/language_movie.csv'
+    MOVIES_CACHE = './cache/movies.csv'
